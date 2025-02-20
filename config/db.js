7131b6a65027376e40e0047f0df8c6bab5064979
dezzyw <dmaulani90@gmail.com>
2025-02-20 09:30:14 +0700

    category

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'kategori'
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Database connection failed:', err.message);
+    return;
+  }
+  console.log('Connected to the database.');
+});
+
+module.exports = db;
